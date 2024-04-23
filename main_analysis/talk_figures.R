9e5e687f418521d696c39467b3f530c0666d6d15
Nicholas Judd <nickkjudd@gmail.com>
2024-04-23 10:23:32 +0200

    added figures for the talk

--- main_analysis/talk_figures.R
+++ main_analysis/talk_figures.R
@@ -57,9 +57,13 @@ p1 <- df_sum %>%
       scale_x_continuous(breaks=c(-120,-60,0,60,120),
                          labels=c("Sept.\n1947", "Sept.\n1952", "Sept.\n1957", "Sept.\n1962", "Sept.\n1967")) +
       ylim(c(.75, 1)) +
-      theme_minimal(base_size = 20) +
+      theme_minimal(base_size = 22) +
       theme(axis.text.x= element_text(angle=45), axis.title.x = element_blank())
   }
+
+p1 + geom_vline(xintercept = 0,linetype="dashed")
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/1.png",
+       width = 8, height = 6, bg = "white")
 
 
 p2 <- df_sum %>% 
@@ -71,40 +75,106 @@ p2 <- df_sum %>%
       scale_x_continuous(breaks=c(-120,-60,0,60,120),
                          labels=c("Sept.\n1947", "Sept.\n1952", "Sept.\n1957", "Sept.\n1962", "Sept.\n1967")) +
       ylim(c(.75, 1)) +
-      theme_minimal(base_size = 20) +
+      theme_minimal(base_size = 22) +
       theme(axis.text.x= element_text(angle=45), axis.title.x = element_blank())
   }
+
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/2.png",
+       p2, width = 8, height = 6, bg = "white")
 
 
 # We could just compare to the two groups... do a t-test get our soup
 p2 + 
   geom_smooth(data = subset(df_sum, running_var <0), method = "lm", formula = y~1, color = "black") +
   geom_smooth(data = subset(df_sum, running_var >0), method = "lm", formula = y~1, color = "black")
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/3.png",
+       width = 8, height = 6, bg = "white")
 
-# yet there is a cohort difference going on here
+
 p2 + 
-  geom_point() +
-  geom_smooth(data = subset(df_sum, running_var <0), method = "lm", formula = y~x, color = "darkred") +
-  geom_smooth(data = subset(df_sum, running_var <0 & running_var > -30), method = "lm", formula = y~x, color = "red")
-
-##### could be solved my going as small as possible
-# the local-randomization approach
-# one month window
-p1 + geom_point(data=subset(df_sum, running_var > -2 & running_var < 1), color = "red")
-# five month window
-p1 + geom_point(data=subset(df_sum, running_var > -6 & running_var < 5), color = "red")
-
-
-# yet still has the strong assumption that those two groups are random...
+  geom_smooth(data=subset(df_sum, running_var < 0), method='glm',formula=y~poly(x,4),se=F, color = "darkred")
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/4.1.png",
+       width = 8, height = 6, bg = "white")
 
 # ideally we would know this function
 p2 + 
-  geom_point() +
   geom_smooth(data=subset(df_sum, running_var < 0), method='glm',formula=y~poly(x,4),se=F, color = "red") +
   geom_smooth(data=subset(df_sum, running_var < 0), method='glm',formula=y~poly(x,20),se=F, color = "darkred")
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/4.2.png",
+       width = 8, height = 6, bg = "white")
+
+
+##### could be solved my going as small as possible
+# the local-randomization approach
+
+# five month window
+p1 + 
+  geom_point(data=subset(df_sum, running_var > -13 & running_var < 12), color = "red") +
+  geom_vline(xintercept = 0,linetype="dashed")
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/5.1.png",
+       width = 8, height = 6, bg = "white")
+# one month window
+p1 + geom_point(data=subset(df_sum, running_var > -2 & running_var < 1), color = "red") +
+  geom_vline(xintercept = 0,linetype="dashed")
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/5.2.png",
+       width = 8, height = 6, bg = "white")
+
+# yet still has the strong assumption that those two groups are random...
 
 
 
+
+
+
+ggplot(df_sum, aes(running_var, piEdu16)) +
+  geom_point(color = "blue", alpha = .3) +
+  geom_smooth(data = subset(df_sum, running_var <0 & running_var > -60), method = "lm", formula = y~x, color = "darkred", fill = NA) +
+  geom_smooth(data = subset(df_sum, running_var <0 & running_var > -20), method = "lm", formula = y~x, color = "red", fill = NA) +
+  geom_vline(xintercept = 0,linetype="dashed") +
+  labs(y = bquote('Percent staying until 16'), x = "Date of Birth in Months") + # bquote('Total Surface Area'~(mm^3))
+  scale_x_continuous(breaks=c(-120,-60,0,60,120),
+                     labels=c("Sept.\n1947", "Sept.\n1952", "Sept.\n1957", "Sept.\n1962", "Sept.\n1967")) +
+  ylim(c(.75, 1)) +
+  theme_minimal(base_size = 20) +
+  theme(axis.text.x= element_text(angle=45), axis.title.x = element_blank())
+  
+
+
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/6.png",
+       width = 8, height = 6, bg = "white")
+
+
+ggplot(df_sum, aes(running_var, piEdu16)) +
+  geom_point(data=subset(df_sum, running_var > -20 & running_var < 20), color = "darkblue") +
+  geom_point(color = "blue", alpha = .3) +
+  geom_smooth(data = subset(df_sum, running_var >0 & running_var < 20), method = "lm", formula = y~x, color = "red", fill = NA) +
+  geom_smooth(data = subset(df_sum, running_var <0 & running_var > -20), method = "lm", formula = y~x, color = "red", fill = NA) +
+  geom_vline(xintercept = 0,linetype="dashed") +
+  labs(y = bquote('Percent staying until 16'), x = "Date of Birth in Months") + # bquote('Total Surface Area'~(mm^3))
+  scale_x_continuous(breaks=c(-120,-60,0,60,120),
+                     labels=c("Sept.\n1947", "Sept.\n1952", "Sept.\n1957", "Sept.\n1962", "Sept.\n1967")) +
+  ylim(c(.75, 1)) +
+  theme_minimal(base_size = 35) +
+  theme(axis.text.x= element_text(angle=45), axis.title.x = element_blank())
+
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/7.1_cont.png",
+       width = 8, height = 6, bg = "white")
+
+
+ggplot(df_sum, aes(running_var, piEdu16)) +
+  geom_point(data=subset(df_sum, running_var > -20 & running_var < 0), color = "darkblue") +
+  geom_point(data=subset(df_sum, running_var > -1 & running_var < 20), color = "darkgreen") +
+  geom_point(data=subset(df_sum, running_var < -20 | running_var > 20), color = "blue", alpha = .3) +
+  geom_vline(xintercept = 0,linetype="dashed") +
+  labs(y = bquote('Percent staying until 16'), x = "Date of Birth in Months") + # bquote('Total Surface Area'~(mm^3))
+  scale_x_continuous(breaks=c(-120,-60,0,60,120),
+                     labels=c("Sept.\n1947", "Sept.\n1952", "Sept.\n1957", "Sept.\n1962", "Sept.\n1967")) +
+  ylim(c(.75, 1)) +
+  theme_minimal(base_size = 35) +
+  theme(axis.text.x= element_text(angle=45), axis.title.x = element_blank())
+
+ggsave("~/Google Drive/My Drive/Assembled Chaos/10 Projects/10.02 ROSLA UK BioBank/presentations/pres_figs/7.2_local.png",
+       width = 8, height = 6, bg = "white")
 
 
 
